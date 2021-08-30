d035d2ed2f2e6795730e44b3500db87afd10c467
sharminmoon <sharminmoon2@gmail.com>
2021-08-30 23:16:30 +0600

    todo routing defination add

---
+++ routes/todo.route.js
@@ -0,0 +1,34 @@
+const { Router } = require("express");
+
+
+const router = Router();
+
+//api for save todo
+router.post('/api/v1.0.0/todos',async(req,res)=>{
+    
+});
+
+
+//api for get todos
+router.get('/api/v1.0.0/todos',async(req,res)=>{
+    
+});
+
+
+
+//api for get todo
+router.get('/api/v1.0.0/todos/:id',async(req,res)=>{
+    
+});
+
+//api for Patch todo
+router.patch('/api/v1.0.0/todos/:id',async(req,res)=>{
+    
+});
+
+
+
+//api for delete todo
+router.delete('/api/v1.0.0/todos/:id',async(req,res)=>{
+    
+});
